6eb854f14938d9eaadb2eecbb68e4b4325b402ea
Вадим <avamim@mail.ru>
2025-07-16 21:10:31 +0300

    iter 5

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-sprint1-cohort44
 go 1.24
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.2
 	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
--- internal/cfg/config.go
+++ internal/cfg/config.go
@@ -1,20 +1,28 @@
 package cfg
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
 
 type Config struct {
-	ServerAddr string
-	BaseURL    string
+	ServerAddr string `env:"SERVER_ADDRESS"`
+	BaseURL    string `env:"BASE_URL"`
 }
 
 var cfg *Config
 
 func init() {
 	cfg = &Config{}
-	flag.Parse()
+	if err := env.Parse(&cfg); err != nil {
+		log.Println("no environment variables found")
+		flag.Parse()
 
-	flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "адрес сервера")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "базовый адрес")
+		flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "адрес сервера")
+		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "базовый адрес")
+		log.Println(cfg.ServerAddr)
+	}
 }
 
 func GetConfigData() *Config {
